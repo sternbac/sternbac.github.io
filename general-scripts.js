b62af313174a4a61ece98f440120cc87e5eb02a8
Edwin Argudo <ea1676@nyu.edu>
2019-08-14 15:41:50 -0400

    added animations

--- general-scripts.js
+++ general-scripts.js
@@ -70,8 +70,10 @@ const body = document.getElementById('body');
 const logoText = document.getElementById('logo-text');
 const centerLogo = document.getElementById('center-logo');
 const navbar = document.getElementById('navbar');
+const navbarHeader = document.getElementById('navbar-header');
 const navItems = document.getElementById("nav-items");
-let movement = ( ( window.innerWidth - centerLogo.offsetWidth ) / 2 ) - 15;
+
+const movement = ( ( window.innerWidth - centerLogo.offsetWidth ) / 2 ) - 15;
 let currScroll = 0;
 
 body.addEventListener('scroll', () => {
@@ -80,15 +82,55 @@ body.addEventListener('scroll', () => {
     if(currScroll > threshold){
         logoText.style.display = "none";
         centerLogo.style.transform = `scale(1) translateX(-${movement}px)`;
-        navbar.style.height = "75px";
-        navItems.style["margin-top"] = "15px";
-        navbar.style["border-bottom"] = "2px solid #374C60";
+        navbarHeader.style.height = "75px";
+        navbarHeader.style["border-bottom"] = "2px solid #374C60";
 
     } else if(currScroll < threshold){
         logoText.style.display = "block";
         centerLogo.style.transform = "scale(1.5) translateX(0px)";
-        navbar.style.height = "120px";
-        navItems.style["margin-top"] = "30px";
-        navbar.style["border-bottom"] = "2px solid transparent";
+        navbarHeader.style.height = "120px";
+        navbarHeader.style["border-bottom"] = "2px solid transparent";
     }
+    fadeOnScroll();
 });
+
+function fadeOnScroll(){
+    const children = document.querySelectorAll(".text");
+    const bodyViewport = body.clientHeight - 20; // -20 cuz padding
+    for(let i = 0; i < children.length; i++){
+        let currChild = children[i];
+        if(!currChild.classList.contains("exposed")){
+            let elemOffset = currChild.getBoundingClientRect()["top"] + body.scrollTop;
+            let bodyOffset = body.scrollTop + bodyViewport;
+            if(elemOffset < bodyOffset){
+                currChild.classList.add("exposed");
+                currChild.animate([
+                    { opacity: 0 },
+                    { opacity: 1 }
+                ], {
+                    fill: 'both',
+                    duration: 1000
+                });
+            }
+        }
+    }
+}
+
+function slideIn(){
+    const contentBlocks = document.querySelectorAll(".content-block");
+    for(let i = 0; i < contentBlocks.length; i++){
+        contentBlocks[i].animate([
+            { right:"100%" },
+            { right:0 }
+        ], {
+            fill: 'both',
+            easing: 'cubic-bezier(.08,.54,.1,1)',
+            duration: 1000
+        });
+    }
+}
+
+(() => {
+    fadeOnScroll();
+    slideIn();
+})();
--- homepage-scripts.js
+++ homepage-scripts.js
@@ -8,17 +8,53 @@ subscribeBtn.addEventListener('click', (event) => {
     handleEmailSubmit(event);
 });
 
+const time = 1000 * 10;
+
+function animateBtn() {
+    subscribeBtn.animate([
+        { backgroundPosition: "0% 0%" },
+        { backgroundPosition: "100% 100%" }
+    ], {
+        fill: 'both',
+        duration: time
+    });
+}
+
 function handleEmailSubmit(event){
     event.preventDefault();
-    const time = 1000 * 10;
-    subscribeBtn.animate([
-            { backgroundPosition: "0% 0%" },
-            { backgroundPosition: "100% 100%" }
-        ], {
-            fill: 'both',
-            duration: time
-        });
+    animateBtn();
+    setInterval(() => {
+        animateBtn();
+    }, time);
+    
     thanksSub.style.display = "block";
     subscribeEmail.style.display = "none";
     subscribeBtnSpan.style.display = "none";
 }
+
+const signUpBtn = document.querySelector("#signup-link");
+const subscribeContainer = document.querySelector("#subscribe-container");
+const subscribeBtnOffset = subscribeBtn.getBoundingClientRect()["bottom"] + 100;
+
+signUpBtn.addEventListener('click', () => {
+    scroll();
+});
+
+function scroll() {
+    if ('requestAnimationFrame' in window === false) {
+        body.scrollTo(0, body.scrollHeight);
+        return;
+    }
+
+    body.scrollTo(0, body.scrollTop + 50);
+
+    const bodyViewport = body.clientHeight - 20; // -20 cuz padding
+    let bodyOffset = body.scrollTop + bodyViewport;
+
+    if(subscribeBtnOffset <= bodyOffset){
+        return;
+    } else {
+        requestAnimationFrame(scroll);
+    }
+}
+
