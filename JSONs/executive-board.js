f0b48b1a39b21b281f51dc8c9aa218e81ed1c3ab
Edwin Argudo <ea1676@nyu.edu>
2020-02-04 09:31:27 -0500

    updated eboard description 2/4

--- JSONs/executive-board.js
+++ JSONs/executive-board.js
@@ -1,91 +1,3 @@
-//var board = {
-//	"board":[
-//		{
-//			"name": "Caitlyn Lubas",
-//			"title": "President",
-//			"school": "Stern Class of 2020",
-//			"degree": "B.S. Marketing and Global Business",
-//			"img": "caitlyn.jpg"
-//		},
-//		{
-//			"name": "Lawrence Langman",
-//			"title": "Vice President",
-//			"school": "Stern Class of 2021",
-//			"degree": "B.S. Finance",
-//			"img": "lawrence.png"
-//		},
-//		{
-//			"name": "Nicole Ng",
-//			"title": "VP of Insight Team",
-//			"school": "Stern Class of 2020",
-//			"degree": "B.S. Data Science and Statistics",
-//			"img": "nicole.jpg"
-//		},
-//		{
-//			"name": "Jonathan Edelstein",
-//			"title": "VP of Outreach",
-//			"school": "Stern Class of 2021",
-//			"degree": "B.S. Finance & Computing and Data Science",
-//			"img": "jonathan.jpg"
-//		},
-//		{
-//			"name": "Neila Zniber",
-//			"title": "VP of Operations",
-//			"school": "Stern Class of 2021",
-//			"degree": "B.S. Finance & Data Science",
-//			"img": "neila.jpg"
-//		},
-//		{
-//			"name": "Virpartap Grewal",
-//			"title": "VP of TechTalks",
-//			"school": "Stern Class of 2020",
-//			"degree": "B.S. Finance & Data Science",
-//			"img": "virpartap.jpg"
-//		},
-//		{
-//			"name": "Abhinav Reddy",
-//			"title": "VP of Workshops",
-//			"school": "Stern Class of 2020",
-//			"degree": "B.S. Finance & Data Science",
-//			"img": "abhinav.jpg"
-//		},
-//		{
-//			"name": "Adwyn Viera",
-//			"title": "Co-VP of Marketing",
-//			"school": "Stern Class of 2022",
-//			"degree": "B.S. Finance",
-//			"img": "adwyn.jpg"
-//		},
-//		{
-//			"name": "Zach Asato",
-//			"title": "Co-VP of Marketing",
-//			"school": "Stern Class of 2020",
-//			"degree": "B.S. Marketing & Finance",
-//			"img": "zach.jpg"
-//		},
-//		{
-//			"name": "Nihar Shah",
-//			"title": "Treasurer",
-//			"school": "Stern Class of 2020",
-//			"degree": "B.S. Marketing & Data Science",
-//			"img": "nihar.jpg"
-//		},
-//		{
-//			"name": "Aj Subudhi",
-//			"title": "Secretary",
-//			"school": "Stern Class of 2021",
-//			"degree": "B.S. Finance & Data Science",
-//			"img": "aj.jpg"
-//		},
-//		{
-//			"name": "Edwin Argudo",
-//			"title": "Webmaster",
-//			"school": "CAS Class of 2020",
-//			"degree": "B.S. Computer Science & Mathematics",
-//			"img": "edwin.jpg"
-//		}
-//	]
-//}
 var board = {
 	"board": [
         {
@@ -119,7 +31,7 @@ var board = {
 		{
 			"name": "Jack Wang",
 			"title": "VP of Outreach",
-			"school": "Stern Class of 2021",
+			"school": "Stern Class of 2023",
 			"degree": "B.S. Finance & Computing and Data Science",
 			"img": "jack.jpg"
 		},
