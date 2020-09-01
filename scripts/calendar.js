eb78a5c855d36837cccd554dc39550aa056d826b
janethuangg <jh6248@nyu.edu>
2020-08-31 23:24:59 -0400

    update location

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,24 +1,48 @@
-const activeMonths = [ 'February', 'March', 'April', 'September', 'October', 'November', 'December'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const activeMonths = [
+  "February",
+  "March",
+  "April",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 function get_month_name(monthNumber) {
-    return months[monthNumber];
+  return months[monthNumber];
 }
 
 (() => {
-    const api = "//bac-backend.herokuapp.com/calendar/0";
-    const req = new XMLHttpRequest();
+  const api = "//bac-backend.herokuapp.com/calendar/0";
+  const req = new XMLHttpRequest();
 
-    let monthDict = {};
+  let monthDict = {};
 
-    req.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const response = JSON.parse(this.responseText);
-            const items = response["data"];
-            for(let i = 0; i < items.length; i++){
-                let entry = items[i];
-                if(activeMonths.includes(entry.month) && entry.title !== "Weekly BAC Meeting"){
-                    let _monthContainer = document.getElementById(`${entry.month}`);
-                    let eventString = `
+  req.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const response = JSON.parse(this.responseText);
+      const items = response["data"];
+      for (let i = 0; i < items.length; i++) {
+        let entry = items[i];
+        if (
+          activeMonths.includes(entry.month) &&
+          entry.title !== "Weekly BAC Meeting"
+        ) {
+          let _monthContainer = document.getElementById(`${entry.month}`);
+          let eventString = `
                         <div class="content-block-container">
                             <div class="content-block">
                                 <div class="content-block-header">
@@ -27,46 +51,45 @@ function get_month_name(monthNumber) {
                                 <div class="content-block-body">
                         `;
 
-                    if(entry.description !== undefined){
-                        eventString += `
+          if (entry.description !== undefined) {
+            eventString += `
                             ${entry.description}
                             <br>
                             <br>
                         `;
-                    }
+          }
 
-                    eventString += `
+          eventString += `
                                 ${entry.time}
                                 <br>
                                 ${entry.location}
                             </div>
                         </div>
                     </div>`;
-                    _monthContainer.insertAdjacentHTML('beforeend', eventString);
-                }
-            }
+          _monthContainer.insertAdjacentHTML("beforeend", eventString);
         }
-    };
+      }
+    }
+  };
 
-    req.open('GET', api);
-    req.send();
+  req.open("GET", api);
+  req.send();
 })();
-
 
 const semesters = document.querySelectorAll(".semester > .semester-header");
 semesters.forEach((s) => {
-    s.addEventListener('click', () => {
-        s.classList.toggle("pressed-semester");
-        let sem = s.dataset.sem;
-        let months = document.querySelector(`#${sem} .months`);
-        let icon = document.querySelector(`#${sem} .semester-icon`);
+  s.addEventListener("click", () => {
+    s.classList.toggle("pressed-semester");
+    let sem = s.dataset.sem;
+    let months = document.querySelector(`#${sem} .months`);
+    let icon = document.querySelector(`#${sem} .semester-icon`);
 
-        if(!s.classList.contains("pressed-semester")){
-            icon.innerHTML = "+";
-            months.classList.remove("showing");
-        } else {
-            icon.innerHTML = "-";
-            months.classList.add("showing");
-        }
-    });
+    if (!s.classList.contains("pressed-semester")) {
+      icon.innerHTML = "+";
+      months.classList.remove("showing");
+    } else {
+      icon.innerHTML = "-";
+      months.classList.add("showing");
+    }
+  });
 });
--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,21 +1,19 @@
-
 (() => {
+  const api = "//bac-backend.herokuapp.com/calendar/3";
+  const req = new XMLHttpRequest();
 
-    const api = "//bac-backend.herokuapp.com/calendar/3";
-    const req = new XMLHttpRequest();
-    
-    const loading = document.querySelector(".loading");
+  const loading = document.querySelector(".loading");
 
-    req.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const response = JSON.parse(this.responseText);
-            const items = response["data"];
-            const eventsContainer = document.getElementById("events-content");
-            loading.style.display = 'none';
-            for(let i = 0; i < items.length; i++){
-                const item = items[i];
+  req.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const response = JSON.parse(this.responseText);
+      const items = response["data"];
+      const eventsContainer = document.getElementById("events-content");
+      loading.style.display = "none";
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
 
-                let itemString = `
+        let itemString = `
                     <div class="content-block-container">
                         <div class="overlay"></div>
                         <div class="content-block">
@@ -25,41 +23,43 @@
                             <div class="content-block-body">
                     `;
 
-                if(item.description !== undefined){
-                    itemString += `
+        if (item.description !== undefined) {
+          itemString += `
                         ${item.description}
                         <br>
                         <br>
                     `;
-                }
+        }
 
-                itemString += `
+        itemString += `
                             ${item.time}
                             <br>
                             ${item.location}
                         </div>
                     </div>
                 </div>`;
-                eventsContainer.insertAdjacentHTML('beforeend', itemString);
-            }
-        }
-    };
+        eventsContainer.insertAdjacentHTML("beforeend", itemString);
+      }
+    }
+  };
 
-    req.open('GET', api);
-    req.send();
-    loading.style.display = 'block';
+  req.open("GET", api);
+  req.send();
+  loading.style.display = "block";
 
-    const members = board["board"];
-    const membersContainer = document.querySelector("#board-container > .members");
-    for(let i = 0; i < members.length; i++){
-        const member = members[i];
-        
-        let imgStr = ""
-        if (member.img != ""){
-            imgStr =  `<img class="member-img" src="images/board/${member.img}">`
-        }
-        
-        const memberString = `
+  const members = board["board"];
+  const membersContainer = document.querySelector(
+    "#board-container > .members"
+  );
+  for (let i = 0; i < members.length; i++) {
+    const member = members[i];
+
+    let imgStr = "";
+    if (member.img != "") {
+      imgStr = `<img class="member-img" src="images/board/${member.img}">`;
+    }
+
+    const memberString = `
             <div class="member">
             ${imgStr}
             <div class="member-content">
@@ -79,83 +79,79 @@
         </div>
         `;
 
-        membersContainer.insertAdjacentHTML('beforeend', memberString);
-    }
-    
+    membersContainer.insertAdjacentHTML("beforeend", memberString);
+  }
 })();
 
+const subscribeBtn = document.getElementById("subscribe-btn");
+const subscribeBtnSpan = document.querySelector("#subscribe-btn > span");
+const subscribeEmail = document.getElementById("subscribe-input");
+const thanksSub = document.getElementById("thanks-sub");
 
-const subscribeBtn = document.getElementById('subscribe-btn');
-const subscribeBtnSpan = document.querySelector('#subscribe-btn > span');
-const subscribeEmail = document.getElementById('subscribe-input');
-const thanksSub = document.getElementById('thanks-sub');
-
-subscribeBtn.addEventListener('click', (event) => {
-    handleEmailSubmit(event);
+subscribeBtn.addEventListener("click", (event) => {
+  handleEmailSubmit(event);
 });
 
 const time = 1000 * 10;
 
 function animateBtn() {
-    subscribeBtn.animate([
-        { backgroundPosition: "0% 0%" },
-        { backgroundPosition: "100% 100%" }
-    ], {
-        fill: 'both',
-        duration: time
-    });
+  subscribeBtn.animate(
+    [{ backgroundPosition: "0% 0%" }, { backgroundPosition: "100% 100%" }],
+    {
+      fill: "both",
+      duration: time,
+    }
+  );
 }
 
-function handleEmailSubmit(event){
-    event.preventDefault();
-    let doneSubmitting = false;
-    animateBtn();
-    const animation = setInterval(animateBtn, time);
+function handleEmailSubmit(event) {
+  event.preventDefault();
+  let doneSubmitting = false;
+  animateBtn();
+  const animation = setInterval(animateBtn, time);
 
-    const email = document.getElementById("subscribe-input").value;
-    const api = "//bac-backend.herokuapp.com/subscribe/";
-    const req = new XMLHttpRequest();
-    
-    req.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            clearInterval(animation);
-            subscribeBtn.style.display = "none";
-            thanksSub.style.display = "block";
-        }
-    };
+  const email = document.getElementById("subscribe-input").value;
+  const api = "//bac-backend.herokuapp.com/subscribe/";
+  const req = new XMLHttpRequest();
 
-    req.open('POST', api, true);
-    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    req.send(`email=${email}`);
-    subscribeEmail.style.display = "none";
-    subscribeBtnSpan.style.display = "none";
+  req.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      clearInterval(animation);
+      subscribeBtn.style.display = "none";
+      thanksSub.style.display = "block";
+    }
+  };
+
+  req.open("POST", api, true);
+  req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  req.send(`email=${email}`);
+  subscribeEmail.style.display = "none";
+  subscribeBtnSpan.style.display = "none";
 }
 
 const signUpBtn = document.querySelector("#signup-link");
 const subscribeContainer = document.querySelector("#subscribe-container");
 
-signUpBtn.addEventListener('click', () => {
-    scroll();
+signUpBtn.addEventListener("click", () => {
+  scroll();
 });
 
 function scroll() {
-    if ('requestAnimationFrame' in window === false) {
-        body.scrollTo(0, body.scrollHeight);
-        return;
-    }
+  if ("requestAnimationFrame" in window === false) {
+    body.scrollTo(0, body.scrollHeight);
+    return;
+  }
 
-    const subscribeBtnOffset = body.scrollHeight;
+  const subscribeBtnOffset = body.scrollHeight;
 
-    body.scrollTo(0, body.scrollTop + 50);
+  body.scrollTo(0, body.scrollTop + 50);
 
-    const bodyViewport = body.clientHeight;
-    let bodyOffset = body.scrollTop + bodyViewport;
+  const bodyViewport = body.clientHeight;
+  let bodyOffset = body.scrollTop + bodyViewport;
 
-    if(subscribeBtnOffset <= bodyOffset){
-        return;
-    } else {
-        requestAnimationFrame(scroll);
-    }
+  if (subscribeBtnOffset <= bodyOffset) {
+    return;
+  } else {
+    requestAnimationFrame(scroll);
+  }
 }
-
-
